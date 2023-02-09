2a804bd07af4e9df7c2ecf88e27a100fed7335df
Hanno Hildenbrandt <h.hildenbrandt@rug.nl>
2023-02-09 22:42:25 +0100

    nop

--- installRserver.sh
+++ installRserver.sh
@@ -10,5 +10,5 @@ sudo apt-get install gdebi-core
 wget https://download2.rstudio.org/server/bionic/amd64/rstudio-server-2022.12.0-353-amd64.deb
 sudo gdebi rstudio-server-2022.12.0-353-amd64.deb
 
-# better use our R install
+# better use our own R install
 echo "rsession-which-r=$HOME/opt/bin/R" | sudo tee -a /etc/rstudio/rserver.conf
