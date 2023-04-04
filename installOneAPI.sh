19deb718d675b3768426f6cce7165d7ce863db9b
Hanno Hildenbrandt <h.hildenbrandt@rug.nl>
2023-04-04 18:05:16 +0200

    oneapi debian

--- installOneAPI.sh
+++ installOneAPI.sh
@@ -3,7 +3,7 @@
 # Installs Intel's OneAPI
 # Hanno 2023
 
-wget -O- https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB | gpg --dearmor | sudo tee /usr/share/keyrings/oneapi-archive-keyring.gpg > dev/null
+wget -O- https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB | gpg --dearmor | sudo tee /usr/share/keyrings/oneapi-archive-keyring.gpg > /dev/null
 echo "deb [signed-by=/usr/share/keyrings/oneapi-archive-keyring.gpg] https://apt.repos.intel.com/oneapi all main" | sudo tee /etc/apt/sources.list.d/oneAPI.list
 
 sudo apt update
